6c6239cad03736f5148c100f5f812bfc5999501c
Oussama Diai <oussama-diaipro@protonmail.com>
2023-06-27 14:42:19 +0200

    added color distinction between the digits of the puzzle and the solution ones

--- gui.py
+++ gui.py
@@ -1,11 +1,13 @@
 import tkinter as tk
-from sudoku_solver import solve_sudoku, print_grid
+from sudoku_solver import solve_sudoku
 
 def solve():
     # Résoudre la grille de Sudoku
     grid = read_grid_from_gui()
-    solve_sudoku(grid)
-    update_gui_from_grid(grid)
+    if solve_sudoku(grid):
+        update_gui_from_grid(grid)
+    else:
+        print("La grille n'a pas de solution.")
 
 def read_grid_from_gui():
     # Lire la grille de Sudoku à partir de l'interface graphique
@@ -15,9 +17,9 @@ def read_grid_from_gui():
         for j in range(9):
             cell_value = cells[i][j].get()
             if cell_value.isdigit():
-                row.append(int(cell_value))
+                row.append((int(cell_value), "blue"))
             else:
-                row.append(0)
+                row.append((0, "black"))
         grid.append(row)
     return grid
 
@@ -25,14 +27,17 @@ def update_gui_from_grid(grid):
     # Mettre à jour l'interface graphique avec la grille résolue
     for i in range(9):
         for j in range(9):
+            cell_value, cell_color = grid[i][j]
             cells[i][j].delete(0, tk.END)
-            cells[i][j].insert(0, str(grid[i][j]))
+            cells[i][j].insert(0, str(cell_value))
+            cells[i][j].config(fg=cell_color)
 
 def clear():
     # Effacer les valeurs de la grille
     for i in range(9):
         for j in range(9):
             cells[i][j].delete(0, tk.END)
+            cells[i][j].config(fg="black")
 
 def create_grid_cells(root):
     # Créer les cellules de la grille dans l'interface graphique
@@ -40,7 +45,7 @@ def create_grid_cells(root):
     for i in range(9):
         row = []
         for j in range(9):
-            cell = tk.Entry(root, width=2, font=("Arial", 20))
+            cell = tk.Entry(root, width=2, font=("Arial", 20), justify="center")
             cell.grid(row=i, column=j)
             row.append(cell)
         cells.append(row)
@@ -61,4 +66,3 @@ clear_button.grid(row=9, column=5, columnspan=4, padx=10, pady=5)
 
 # Lancer la boucle principale de l'interface graphique
 root.mainloop()
-
